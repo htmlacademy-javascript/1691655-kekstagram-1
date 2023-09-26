e569b1292972ee95bbcc7fd39591516634483c7c
vetrov78 <vettrov78@gmail.com>
2023-09-26 08:24:40 +0300

    Отображение комментариев

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,23 +1,40 @@
 import { posts } from './pictures.js';
 
+const ADD_POSTS_NUMBER = 5;
+
 const picturesLinkList = document.querySelectorAll('.picture');
 const bigPicture = document.querySelector('.big-picture');
+const loadMoreButton = bigPicture.querySelector('.comments-loader');
+const commentsList = bigPicture.querySelector('.social__comments');
 
+<<<<<<< HEAD
+=======
+// Логика закрытия полноразмерного изображения
+const toDefaultValues = () => {
+  bigPicture.classList.add('hidden');
+  document.querySelector('body').classList.remove('modal-open');
+  loadMoreButton.classList.remove('hidden');
+  commentsList.innerHTML = '';
+};
+
+>>>>>>> b128cdc (Отображение комментариев)
 const onEscRemove = (evt) => {
   if (evt.key === 'Escape' || evt.key === 'Esc') {
     evt.preventDefault();
-    bigPicture.classList.add('hidden');
-    document.querySelector('body').classList.remove('modal-open');
-
+    toDefaultValues();
     document.removeEventListener('keydown', onEscRemove);
   }
 };
 
 bigPicture.querySelector('#picture-cancel').addEventListener('click', () => {
-  bigPicture.classList.add('hidden');
-  document.querySelector('body').classList.remove('modal-open');
+  toDefaultValues();
+  document.removeEventListener('keydown', onEscRemove);
 });
 
+<<<<<<< HEAD
+=======
+// Логика отображения полноразмерного изображения
+>>>>>>> b128cdc (Отображение комментариев)
 picturesLinkList.forEach((picture) => {
   picture.addEventListener('click', () => {
     bigPicture.classList.remove('hidden');
@@ -30,25 +47,57 @@ picturesLinkList.forEach((picture) => {
       .src = picture.getElementsByTagName('img')[0].src;
 
     bigPicture.querySelector('.likes-count').textContent = picture.querySelector('.picture__likes').textContent;
-    bigPicture.querySelector('.comments-count').textContent = picture.querySelector('.picture__comments').textContent;
     bigPicture.querySelector('.social__caption').textContent = picture;
 
     const commentTemplate = document.querySelector('#comment').content;
     const commentItemTemplate = commentTemplate.querySelector('li');
-    const commentsList = bigPicture.querySelector('.social__comments');
 
     commentsList.innerHTML = '';
     const currentPost = posts.find((post) => post.id === parseInt(picture.id, 10));
-    currentPost
-      .comments
-      .forEach((comment) => {
-        const commentElement = commentItemTemplate.cloneNode(true);
-        commentElement.querySelector('.social__picture').src = comment.avatar;
-        commentElement.querySelector('.social__picture').alt = comment.name;
-        commentElement.querySelector('.social__text').textContent = comment.message;
-        commentsList.appendChild(commentElement);
-      });
+    const commentsNumber = currentPost.comments.length;
+
     bigPicture.querySelector('.social__caption').textContent = currentPost.description;
+
+    // Отображение комментариев
+    const showNewComment = (comment) => {
+      const commentElement = commentItemTemplate.cloneNode(true);
+      commentElement.querySelector('.social__picture').src = comment.avatar;
+      commentElement.querySelector('.social__picture').alt = comment.name;
+      commentElement.querySelector('.social__text').textContent = comment.message;
+      commentsList.appendChild(commentElement);
+    };
+
+    const showedCommentsNumber = (commentsNumber <= ADD_POSTS_NUMBER) ? commentsNumber : ADD_POSTS_NUMBER;
+    const htmlFragment = `${showedCommentsNumber} из <span class="comments-count">${commentsNumber}</span> комментариев`;
+
+    bigPicture.querySelector('.social__comment-count').innerHTML = htmlFragment;
+    if (commentsNumber <= ADD_POSTS_NUMBER) {
+      loadMoreButton.classList.add('hidden');
+      currentPost
+        .comments
+        .forEach((comment) => {
+          showNewComment(comment);
+        });
+    } else {
+      currentPost
+        .comments
+        .slice(0, ADD_POSTS_NUMBER)
+        .forEach((comment) => {
+          showNewComment(comment);
+        });
+      loadMoreButton.addEventListener('click', () => {
+        console.log('click');
+
+        if (commentsNumber - showedCommentsNumber <= ADD_POSTS_NUMBER) {
+          currentPost
+            .comments
+            .slice(showedCommentsNumber, commentsNumber)
+            .forEach((comment) => {
+              showNewComment(comment);
+            });
+        }
+      });
+    }
   });
 });
 
