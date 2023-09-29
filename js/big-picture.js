970f599d2b15fda71ee093370632409ef6485989
vetrov78 <vettrov78@gmail.com>
2023-09-29 20:53:07 +0300

    Добавляет логику кнопки загрузки еще комментариев

--- js/big-picture.js
+++ js/big-picture.js
@@ -7,8 +7,6 @@ const bigPicture = document.querySelector('.big-picture');
 const loadMoreButton = bigPicture.querySelector('.comments-loader');
 const commentsList = bigPicture.querySelector('.social__comments');
 
-<<<<<<< HEAD
-=======
 // Логика закрытия полноразмерного изображения
 const toDefaultValues = () => {
   bigPicture.classList.add('hidden');
@@ -17,7 +15,6 @@ const toDefaultValues = () => {
   commentsList.innerHTML = '';
 };
 
->>>>>>> b128cdc (Отображение комментариев)
 const onEscRemove = (evt) => {
   if (evt.key === 'Escape' || evt.key === 'Esc') {
     evt.preventDefault();
@@ -31,34 +28,31 @@ bigPicture.querySelector('#picture-cancel').addEventListener('click', () => {
   document.removeEventListener('keydown', onEscRemove);
 });
 
-<<<<<<< HEAD
-=======
 // Логика отображения полноразмерного изображения
->>>>>>> b128cdc (Отображение комментариев)
 picturesLinkList.forEach((picture) => {
   picture.addEventListener('click', () => {
+    const currentPost = posts.find((post) => post.id === parseInt(picture.id, 10));
+
     bigPicture.classList.remove('hidden');
     document.querySelector('body').classList.add('modal-open');
     document.addEventListener('keydown', onEscRemove);
 
     bigPicture
-      .querySelector('.big-picture__img')
-      .getElementsByTagName('img')[0]
-      .src = picture.getElementsByTagName('img')[0].src;
-
-    bigPicture.querySelector('.likes-count').textContent = picture.querySelector('.picture__likes').textContent;
-    bigPicture.querySelector('.social__caption').textContent = picture;
+      .querySelector('.big-picture__img img')
+      .src = currentPost.url;
+    bigPicture.querySelector('.likes-count').textContent = currentPost.likes;
+    bigPicture.querySelector('.social__caption').textContent = currentPost.description;
 
     const commentTemplate = document.querySelector('#comment').content;
     const commentItemTemplate = commentTemplate.querySelector('li');
 
     commentsList.innerHTML = '';
-    const currentPost = posts.find((post) => post.id === parseInt(picture.id, 10));
+
     const commentsNumber = currentPost.comments.length;
 
     bigPicture.querySelector('.social__caption').textContent = currentPost.description;
 
-    // Отображение комментариев
+    // Добавление комментария в разметку
     const showNewComment = (comment) => {
       const commentElement = commentItemTemplate.cloneNode(true);
       commentElement.querySelector('.social__picture').src = comment.avatar;
@@ -67,36 +61,49 @@ picturesLinkList.forEach((picture) => {
       commentsList.appendChild(commentElement);
     };
 
-    const showedCommentsNumber = (commentsNumber <= ADD_POSTS_NUMBER) ? commentsNumber : ADD_POSTS_NUMBER;
-    const htmlFragment = `${showedCommentsNumber} из <span class="comments-count">${commentsNumber}</span> комментариев`;
-
+    let showedCommentsNumber = (commentsNumber <= ADD_POSTS_NUMBER) ? commentsNumber : ADD_POSTS_NUMBER;
+    let htmlFragment = (commentsNumber === 0)
+      ? 'комментариев пока нет..'
+      : `${showedCommentsNumber} из <span class="comments-count">${commentsNumber}</span> комментариев`;
     bigPicture.querySelector('.social__comment-count').innerHTML = htmlFragment;
+
+    // Вывод комментариев
     if (commentsNumber <= ADD_POSTS_NUMBER) {
       loadMoreButton.classList.add('hidden');
-      currentPost
-        .comments
-        .forEach((comment) => {
-          showNewComment(comment);
-        });
-    } else {
-      currentPost
-        .comments
-        .slice(0, ADD_POSTS_NUMBER)
-        .forEach((comment) => {
-          showNewComment(comment);
-        });
-      loadMoreButton.addEventListener('click', () => {
-        console.log('click');
-
-        if (commentsNumber - showedCommentsNumber <= ADD_POSTS_NUMBER) {
-          currentPost
-            .comments
-            .slice(showedCommentsNumber, commentsNumber)
-            .forEach((comment) => {
-              showNewComment(comment);
-            });
-        }
+      currentPost.comments.forEach((comment) => {
+        showNewComment(comment);
       });
+    } else {
+      currentPost.comments.slice(0, ADD_POSTS_NUMBER).forEach((comment) => {
+        showNewComment(comment);
+      });
+      // При нажатии на Загрузить еще
+      const handleMoreCommentsClick = () => {
+        // Если осталось загрузить последнюю порцию
+        if (commentsNumber - showedCommentsNumber <= ADD_POSTS_NUMBER) {
+
+          loadMoreButton.classList.add('hidden');
+          loadMoreButton.removeEventListener('click', handleMoreCommentsClick);
+
+          currentPost.comments.slice(showedCommentsNumber, commentsNumber).forEach((comment) => {
+            showNewComment(comment);
+          });
+          showedCommentsNumber += commentsNumber - showedCommentsNumber;
+          htmlFragment = `${showedCommentsNumber} из <span class="comments-count">${commentsNumber}</span> комментариев`;
+          bigPicture.querySelector('.social__comment-count').innerHTML = htmlFragment;
+
+        // Если - порций осталось несколько
+        } else {
+
+          currentPost.comments.slice(showedCommentsNumber, showedCommentsNumber + ADD_POSTS_NUMBER).forEach((comment) => {
+            showNewComment(comment);
+          });
+          showedCommentsNumber += ADD_POSTS_NUMBER;
+          htmlFragment = `${showedCommentsNumber} из <span class="comments-count">${commentsNumber}</span> комментариев`;
+          bigPicture.querySelector('.social__comment-count').innerHTML = htmlFragment;
+        }
+      };
+      loadMoreButton.addEventListener('click', handleMoreCommentsClick);
     }
   });
 });
